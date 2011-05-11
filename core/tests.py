5b8d5c645861bf71e541af9de458696cc581d689
Cleiton Jr Mittmann <mittmannv8@gmail.com>
2011-05-10 22:59:57 -0300

    Hotsite eventex criado

---
+++ core/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+class IndexUrlTest(TestCase):
+    def test_success_when_get_homepage(self):
+        response = self.client.get('/')
+        self.assertEquals(200, response.status_code)
+        self.assertTemplateUsed(response, 'index.html')
+
---
+++ urls.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+# Importa método responsavel pela página inicial
+from core.views import index
+
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'eventex.views.home', name='home'),
+    # url(r'^eventex/', include('eventex.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+
+    (r'^$', index),
+
+)
+
+# Static URL
+urlpatterns += staticfiles_urlpatterns()
+
